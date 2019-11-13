c415eeb9fe8b019fae70475e3c92d2c05d2389c3
olicarignan <olive.carignan@gmail.com>
2019-11-13 19:33:08 +0000

    category authentication form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,9 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                               password: ENV["HTTP_BASIC_PASSWORD"],
+                               if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
+
   def create
     @category = Category.new(category_params)
 
