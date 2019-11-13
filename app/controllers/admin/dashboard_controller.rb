1bedf5f8d3c8d16b4b8f042ea20584d039e46afe
olicarignan <olive.carignan@gmail.com>
2019-11-13 17:41:01 +0000

    admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  
-
 
   def show
+    @product_count = Product.count()
+    @total_product_count = Product.sum(:quantity)
+    @categories_count = Category.count()
   end
 end
